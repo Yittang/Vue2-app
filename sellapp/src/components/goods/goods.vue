fe6fc0868087f68868695762da76d4e1a0c58b74
Yittang <657217449@qq.com>
2018-02-06 22:15:57 +0800

    update goods

--- sellapp/src/components/goods/goods.vue
+++ sellapp/src/components/goods/goods.vue
@@ -1,12 +1,114 @@
 <template>
-	<div>I am goods</div>
+	<div class="wrapper">
+		<div class="menu">
+			<ul class="menu-list">
+				<li v-for="item in goods" class="menu-item">
+					<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+					<span class="text">{{ item.name }}</span>
+				</li>
+			</ul>
+		</div>
+		<div class="food"></div>
+	</div>
 </template>
 
 <script type="ecmascript-6">
-	
-	export default {}
+	const ERR_OK = 0
+
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		},
+		data() {
+			return {
+				goods: []
+			}
+		},
+		created() {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
+
+			this.$axios.get('/api/goods')
+				.then(response => {
+					response = response.data
+					if(response.errno == ERR_OK) {
+						this.goods = response.data
+					}
+				})
+				.catch(error => {
+					console.log(error)
+			})
+		}
+	}
 </script>
 
-<style rel="stylesheet">
-	
+<style rel="stylesheet" lang="less" scoped>
+
+	@import '../../common/style/mixin.less';
+	.wrapper {
+		display: flex;
+		position: absolute;
+		top: 174px;
+		bottom: 48px;
+		width: 100%;
+		overflow: hidden;
+		.menu {
+			width: 80px;
+			background: #f3f5f7;
+			.menu-list {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				height: 100%;
+				.menu-item {					
+					flex: 1;
+					font-size: 0;
+					padding: 12px;
+					.icon {
+						display: inline-block;
+						width: 12px;
+						height: 12px;
+						background-size: 12px;
+						vertical-align: top;
+						margin-right: 2px;
+						&.decrease {
+							.bg-image('../../components/goods/decrease_3');
+						}
+						&.discount {
+							.bg-image('../../components/goods/discount_3');
+						}					
+						&.guarantee {
+							.bg-image('../../components/goods/guarantee_3');
+						}					
+						&.invoice {
+							.bg-image('../../components/goods/invoice_3');
+						}
+						&.special {
+							.bg-image('../../components/goods/special_3');
+						}
+					}
+					.text {
+						position: relative;
+						font-size: 12px;
+						line-height: 14px;
+						color: #000;
+						&::after {
+							position: absolute;
+							bottom: -12px;
+							display: block;
+							content: '';
+							width: 56px;
+							height: 1px;
+							border-bottom: 1px solid rgba(7, 17, 27, .1);
+						}
+					}
+					
+				}
+			}
+		}
+		.food {
+			flex: 1;
+		}
+	}
 </style>
--- sellapp/src/components/header/header.vue
+++ sellapp/src/components/header/header.vue
@@ -27,20 +27,34 @@
 		<div class="background-img">
 			<img :src="seller.avatar" alt="背景" width="100%" height="100%">
 		</div>
-		<div v-show="modalShow" class="modal">
-			<div class="main clearfix">
-				<div class="detail-main">
-					<h1 class="name">{{ seller.name }}</h1>
-					<star :size="48" :score="seller.score"></star>
-					<div class="title">
-						<span class="text">优惠信息</span>
+		<transition name="fade">
+			<div v-show="modalShow" class="modal">
+				<div class="main clearfix">
+					<div class="detail-main">
+						<h1 class="name">{{ seller.name }}</h1>
+						<star :size="48" :score="seller.score"></star>
+						<div class="title">
+							<span class="text">优惠信息</span>
+						</div>
+						<ul v-if="seller.supports" class="supports">
+							<li class="support-item" v-for="(item,index) in seller.supports">
+								<span class="icon" :class="classMap[seller.supports[index].type]"></span>
+								<span class="text">{{seller.supports[index].description}}</span>
+							</li>
+						</ul>
+						<div class="title">
+							<span class="text">商家公告</span>
+						</div>
+						<div class="description">
+							<p>{{ seller.bulletin }}</p>
+						</div>
 					</div>
 				</div>
+				<div class="modal-close" @click="hideModal">
+					<i class="icon-close"></i>
+				</div>
 			</div>
-			<div class="modal-close" @click="hideModal">
-				<i class="icon-close"></i>
-			</div>
-		</div>
+		</transition>
 	</div>
 </template>
 
@@ -260,6 +274,56 @@
 							top: -6px;
 							border-bottom: 1px solid rgba(255, 255, 255, .2);
 						}
+					}
+					.supports {
+						width: 80%;
+						margin: 0 auto;
+						.support-item {
+							padding: 0 12px;
+							margin-bottom: 12px;
+							font-size: 0;
+							&:last-child {
+								margin-bottom: 0;
+							}
+							.icon {
+								display: inline-block;
+								width: 16px;
+								height: 16px;
+								vertical-align: top;
+								margin-right: 6px;
+								background-size: 16px;
+								&.decrease {
+									.bg-image('../../components/header/decrease_2');
+								}
+								&.discount {
+									.bg-image('../../components/header/discount_2');
+								}					
+								&.guarantee {
+									.bg-image('../../components/header/guarantee_2');
+								}					
+								&.invoice {
+									.bg-image('../../components/header/invoice_2');
+								}
+								&.special {
+									.bg-image('../../components/header/special_2');
+								}
+							}
+							.text {
+								font-size: 12px;
+								color: #fff;
+								line-height: 12px;
+							}
+						}	
+					}
+					.description {
+						width: 80%;
+						margin: 0 auto;
+						p {
+							padding: 0 12px;
+							font-size: 12px;
+							color: #fff;
+							line-height: 24px;
+						}
 					}	
 				}
 			}
@@ -273,5 +337,12 @@
 				color: rgba(255, 255, 255, .5);
 			}
 		}
+	}
+
+	.fade-enter-active, .fade-leave-active {
+	    transition: opacity .5s
+	}
+	.fade-enter, .fade-leave-to {
+	    opacity: 0
 	}
 </style>
